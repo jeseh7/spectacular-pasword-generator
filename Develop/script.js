c33616fd58c208d7e5cac33108ce3f3ef55b427d
Jese H <pilers2000@hotmail.com>
2023-06-29 18:36:36 -0400

    test merge

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,11 @@
 // Create all arrays for password character possibilities
 var possibilities = [];
-const special = ["!", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", 
-                  "-", ".", "/", ":", ";", "<", "\"", "=", ">", "?", "@", 
-                  "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+const special = ["!", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "\"", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 const nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n",
-                    "o","p","q","r","s","t","u","v","w","x","y","z"];
-const uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N",
-                    "O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
+var x = 0;
 // Ask for password character count and collect value into a variable. 
 // How many characters do you want your password to be?
 function generatePassword(){
